0999c73e18e771e359a44ade03cf1c183a534265
黎海 <527748667@qq.com>
2017-04-07 17:08:38 +0800

    更新

--- Application/Admin/Controller/ServiceController.class.php
+++ Application/Admin/Controller/ServiceController.class.php
@@ -1,9 +1,12 @@
 <?php
 namespace Admin\Controller;
 use Think\Controller;
-class ServiceController extends Controller {
+class ServiceController extends CommonController {
     public function lists(){
+        $service = D('Service');
+        $lists = $service->order('id desc')->select();        //查询列表
 
+        $this->assign('lists',$lists);
         $this->display();
     }
     public function add(){
@@ -11,12 +14,43 @@ class ServiceController extends Controller {
             $service = D('Service');
             $service->create(); // 创建service数据对象，默认通过表单提交的数据进行创建
             $service->create_time = date('Y-m-d H:i:s');
-
+            if($_FILES['file']['error']==0){
+                $img_url=$this->uploadOne($_FILES['file']);
+            }
+            $service->img_url=$img_url;
+            $service->state=1;
             $service->add();// 把数据对象添加到数据库
-            $this->success('添加完成','Service/lists');
+            $this->success('添加成功',U('Admin/Service/lists'));die;
         }
 
         $this->display();
+    }
+    public function save(){
+        $service = D('Service');
+        $data = $service->where('id='.I('get.id'))->find();
+        //dump($data);die;
+        if(IS_POST){
+            $service->create(); // 创建service数据对象，默认通过表单提交的数据进行创建
+            $service->create_time = date('Y-m-d H:i:s');
+            $img_url=$data['img_url'];
+            if($_FILES['file']['error']==0){
+                $img_url=$this->uploadOne($_FILES['file']);
+            }
+            $service->img_url=$img_url;
+            $service->where('id='.I('get.id'))->save();// 把数据对象添加到数据库
+            $this->success('修改成功',U('Admin/Service/lists'));die;
+        }
+
+        $this->assign('data',$data);
+        $this->display();
+    }
+
+    public function delete(){
+        $service = D('Service');
+        $id=I('get.id');
+        $service->where('id='.$id)->delete();
+
+        $this->redirect('Service/lists');
     }
 
 
--- Application/Runtime/Cache/Admin/b595b808c2ebeeeb0478ffc30bb4e87a.php
+++ Application/Runtime/Cache/Admin/b595b808c2ebeeeb0478ffc30bb4e87a.php
@@ -22,7 +22,7 @@
     <h2><span class="icon-user"></span>基本设置</h2>
     <ul style="display:block">
         <li><a href="<?php echo U('Info/info');?>" target="right"><span class="icon-caret-right"></span>网站设置</a></li>
-        <li><a href="pass.html" target="right"><span class="icon-caret-right"></span>修改密码</a></li>
+        <li><a href="<?php echo U('Pass/pass');?>" target="right"><span class="icon-caret-right"></span>修改密码</a></li>
         <li><a href="page.html" target="right"><span class="icon-caret-right"></span>单页管理</a></li>
         <li><a href="adv.html" target="right"><span class="icon-caret-right"></span>首页轮播</a></li>
         <li><a href="book.html" target="right"><span class="icon-caret-right"></span>留言管理</a></li>
@@ -31,7 +31,7 @@
     <h2><span class="icon-pencil-square-o"></span>栏目管理</h2>
     <ul>
         <li><a href="list.html" target="right"><span class="icon-caret-right"></span>内容管理</a></li>
-        <li><a href="add.html" target="right"><span class="icon-caret-right"></span>添加内容</a></li>
+        <li><a href="<?php echo U('add/add');?>" target="right"><span class="icon-caret-right"></span>添加内容</a></li>
         <li><a href="cate.html" target="right"><span class="icon-caret-right"></span>分类管理</a></li>
     </ul>
 
@@ -47,7 +47,7 @@
     <h2><span class="icon-pencil-square-o"></span>新闻管理</h2>
     <ul>
         <li><a href="<?php echo U('news/news');?>" target="right"><span class="icon-caret-right"></span>内容管理</a></li>
-        <li><a href="add.html" target="right"><span class="icon-caret-right"></span>添加内容</a></li>
+        <li><a href="<?php echo U('news/add');?>" target="right"><span class="icon-caret-right"></span>添加内容</a></li>
         <li><a href="cate.html" target="right"><span class="icon-caret-right"></span>分类管理</a></li>
     </ul>
     <!-- 新闻管理footer -->
@@ -72,7 +72,7 @@
         &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;切换语言：<a href="##">中文</a> &nbsp;&nbsp;<a href="##">英文</a> </li>
 </ul>
 <div class="admin">
-  <iframe scrolling="auto" rameborder="0" src="../Info/info.html" name="right" width="100%" height="100%"></iframe>
+  <iframe scrolling="auto" rameborder="0" src="<?php echo U('info/info');?>" name="right" width="100%" height="100%"></iframe>
 </div>
 <div style="text-align:center;">
 </div>
