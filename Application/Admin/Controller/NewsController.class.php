ef044dba42f211a17f83e2d782e6650e00639647
Oblivion <1094439667@qq.com>
2017-04-10 11:39:40 +0800

    News数据更替加了个表字段

--- Application/Admin/Controller/NewsController.class.php
+++ Application/Admin/Controller/NewsController.class.php
@@ -19,14 +19,26 @@ class NewsController extends CommonController {
         public function news(){
             //初始化数组
             $arr=array();
-            $User = M('news_content'); // 实例化User对象
-            $count      = $User->where($arr)->count();// 查询满足要求的总记录数
+            $news = M('news_content'); // 实例化User对象
+            $count      = $news->where($arr)->count();// 查询满足要求的总记录数
             $Page       = new \Think\Page($count,25);// 实例化分页类 传入总记录数和每页显示的记录数(5)
             $show       = $Page->show();// 分页显示输出
-            $list = $User->where($arr)->order('id')->limit($Page->firstRow.','.$Page->listRows)->select();
+            $list = $news->where($arr)->order('id')->limit($Page->firstRow.','.$Page->listRows)->select();
             $this->assign('list',$list);// 赋值数据集
             $this->assign('page',$show);// 赋值分页输出
             $this->display();
+            /*
+             * 全选删除
+             */
+            if(isset($_POST['id'])){
+                $idd=implode(",",$_POST['id']);
+                $listt = $news->delete($idd);
+                if($listt){
+                    $this->success('删除成功',U('Admin/news/news'));die;
+                }else{
+                    $this->error('删除失败');
+                }
+            }
         }
         /*
          * 新闻添加页
@@ -44,6 +56,7 @@ class NewsController extends CommonController {
                 $data['img_url']=$images;
                 $data['time']=time();
                 $data['jianjie']=$_POST['simple'];
+                $data['pid']=$_POST['pid'];
                 $data['news_url']=$_POST['newsurl'];
                 $list=$news->data($data)->add();
                 if($list){
